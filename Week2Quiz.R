f9a7b7ed9c04369341aa2cf8a27362b690864529
Senbagaraman Manoharan <senbagaraman04@gmail.com>
2016-12-13 18:56:42 +0530

    Create Week2Quiz.R

---
+++ Week2Quiz.R
@@ -0,0 +1,20 @@
+#coursera
+#Name:R Programming 
+#By:Roger D Peng
+#University:John Hopkins University
+
+#Answers to week 2 Quiz
+#1.Suppose I define the following function in R what is the output?
+
+cube <- function(x, n) {
+        x^3
+}
+
+cube(3)  
+
+#2.The following code will produce a warning in R
+x <- 1:10
+if(x > 5) {
+        x <- 0
+}
+
